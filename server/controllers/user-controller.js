688823edb8ff52dfe8fe5c11b2e1d218b2830f19
Roman Petrashkevich <roman.petrashkevich@skdo.systems>
2022-03-04 20:38:24 +0300

    add router

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,51 @@
+class UserController {
+	async registration(req, res, next) {
+		try {
+			
+		} catch (error) {
+			
+		}
+	}
+
+	async login(req, res, next) {
+		try {
+			
+		} catch (error) {
+			
+		}
+	}
+
+	async logout(req, res, next) {
+		try {
+			
+		} catch (error) {
+			
+		}
+	}
+
+	async activate(req, res, next) {
+		try {
+			
+		} catch (error) {
+			
+		}
+	}
+
+	async refresh(req, res, next) {
+		try {
+			
+		} catch (error) {
+			
+		}
+	}
+
+	async getUsers(req, res, next) {
+		try {
+			res.json(['h1llo', 'from', 'node js'])
+		} catch (error) {
+			
+		}
+	}
+}
+
+module.exports = new UserController()
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 
+const router = require('./router')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -12,6 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
+app.use('/api', router)
 
 const start = async () => {
   try {
